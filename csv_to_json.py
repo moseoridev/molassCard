32d2867e598bb5db022a9811f8483ae74a84afeb
moseoridev <sjssjs1344@gmail.com>
2020-09-25 09:21:13 +0000

    2학기 수행평가 업데이트

---
+++ csv_to_json.py
@@ -0,0 +1,13 @@
+import json
+
+
+J = list()
+csv = open("voca.csv", "r")
+
+for line in csv.readlines():
+    a = line.split(",")
+    _t = [a[0], a[1].replace("\n", "")]
+    J.append(_t)
+
+with open('voca.json', 'w') as f:
+    json.dump(J, f, ensure_ascii=False)
