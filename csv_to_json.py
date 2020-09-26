49236ccfc9fcb6cf92434870f0ee5a578d06279f
moseoridev <sjssjs1344@gmail.com>
2020-09-26 14:29:05 +0000

    fix /r

--- csv_to_json.py
+++ csv_to_json.py
@@ -6,7 +6,7 @@ csv = open("voca.csv", "r")
 
 for line in csv.readlines():
     a = line.split(",")
-    _t = [a[0], a[1].replace("\n", "")]
+    _t = [a[0], a[1].replace("\n", "").replace("\r", "")]
     J.append(_t)
 
 with open('voca.json', 'w') as f:
