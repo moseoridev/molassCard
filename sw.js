ada49b72aa834904ab4aeba5b9ecd1aa4f955cef
moseoridev <sjssjs1344@gmail.com>
2021-03-22 19:11:16 +0900

    edit dark mode color

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "my-site-cache-v5";
+var CACHE_NAME = "my-site-cache-v6";
 var urlsToCache = [
   "./index.html",
   "./assets/audio/correct.mp3",
@@ -55,7 +55,7 @@ self.addEventListener("fetch", function (event) {
 });
 
 self.addEventListener("activate", function (event) {
-  var cacheAllowlist = ["my-site-cache-v5"];
+  var cacheAllowlist = ["my-site-cache-v6"];
 
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
